5c141f53b1f41ab707600c23453ddae7a1a79095
Anisha3102 <anisha31102002@gmail.com>
2025-02-28 21:40:51 +0530

    Registering User

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,2 +1,55 @@
-export const registerController = () => {};
+import { hashPassword } from "../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
 
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ error: "email is Required" });
+    }
+    if (!password) {
+      return res.send({ error: "password is Required" });
+    }
+    if (!phone) {
+      return res.send({ error: "phone no. is Required" });
+    }
+    if (!address) {
+      return res.send({ error: "address is Required" });
+    }
+
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(200).send({
+        success: true,
+        message: "Already registered please login",
+      });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Registered Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in registration",
+      error,
+    });
+  }
+};
